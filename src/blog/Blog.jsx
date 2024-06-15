fce9c98631716cc3a087ec46310e1d143ef04d5a
Kennedy2042 <ogbonnakennedy@gmail.com>
2024-06-15 07:22:41 +0100

    donate page started

--- src/blog/Blog.jsx
+++ src/blog/Blog.jsx
@@ -1,8 +1,54 @@
-import './Blog.css'
+import './Blog.css';
+import { IoMdPlay } from 'react-icons/io'
+import blog2 from '../assets/blog2.png'
 
 const Blog = () => {
+
+  const BlogCard = (props) => {
+    return (
+      <div className='blog_card_body'>
+        <div className='blog_card_image_holder'>
+          <img src={blog2} alt="" />
+        </div>
+        <div className='blog_write_holder'>
+          <h1>{props.h1}</h1>
+          <p>{props.p}</p>
+          <h2>{props.btn}</h2>
+        </div>
+      </div>
+    )
+  }
+
+
+
+
+
+
+
   return (
-    <div>Blog</div>
+    <div className='blog_main_body'>
+      <div className="blog_hero_section">
+        <div className="blog_hero_section_content">
+          <h1>Top Stories</h1>
+          <h2>Highlights:</h2>
+          <h2>Previous marathon held</h2>
+          <div className="blog_hero_section_play_container">
+            <IoMdPlay />
+          </div>
+        </div>
+      </div>
+      <div className="blog_title_header">
+        Other Top Stories
+      </div>
+      <div className="blog_card_container">
+        <div className="blog_card_wrapper">
+          <BlogCard h1='NBA PLAYOFF X-FACTOR' p='the NBA is on a ' btn='Read more'/>
+          <BlogCard h1='NBA PLAYOFF X-FACTOR' p='the NBA is on a ' btn='Read more'/>
+          <BlogCard h1='NBA PLAYOFF X-FACTOR' p='the NBA is on a ' btn='Read more'/>
+          <BlogCard h1='NBA PLAYOFF X-FACTOR' p='the NBA is on a ' btn='Read more'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/donate/Donate.jsx
+++ src/donate/Donate.jsx
@@ -2,7 +2,15 @@ import './Donate.css'
 
 const Donate = () => {
   return (
-    <div>Donate</div>
+    <div className='donate_main_body'>
+      <div className="donate_main_wrapper">
+        <div className="donate_wrapper">
+          <h1>Support The Ajegunle City Marathon</h1>
+          <p>All proceeds shall go to support disadvantaged AYP, identified from the marathon activities whom thereafter, will be prioritized and enrolled into the development program interventions of various organizations in consortium and partnership of this project to ensure sustainable impact</p>
+          <h2>Donation Details</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
