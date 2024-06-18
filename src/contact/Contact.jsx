2356a020c53cde5fe77b8d7f3874aa536bae0c14
Kennedy2042 <ogbonnakenedy@gmail.com>
2024-06-18 13:25:58 +0100

    get intouch started almost completed

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -2,7 +2,21 @@ import './Contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact_us_main_body'>
+      <div className="contact_us_hero_wrapper">
+        CONTACT US
+      </div>
+      <div className="get_intouch_wrapper">
+        <div className="get_intouch_body">
+          <div className="get_intouch">
+            <h2>Get in Touch</h2>
+            <h1>RUN WITH US</h1>
+            <p>Our dedicated team is here to help. If you have any questions or need assistance, don’t hesitate to get in touch.</p>
+            <div className="get_intouch_email_wrapper"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
