740d3da44565fb196842fc75a16d1fd0d4257899
Kennedy2042 <ogbonnakennedy@gmail.com>
2024-06-18 13:25:58 +0100

    get intouch started almost completed

--- src/donate/Donate.jsx
+++ src/donate/Donate.jsx
@@ -8,6 +8,40 @@ const Donate = () => {
           <h1>Support The Ajegunle City Marathon</h1>
           <p>All proceeds shall go to support disadvantaged AYP, identified from the marathon activities whom thereafter, will be prioritized and enrolled into the development program interventions of various organizations in consortium and partnership of this project to ensure sustainable impact</p>
           <h2>Donation Details</h2>
+          <div className="donate_donation_option">
+            <p>Donation Option</p>
+            <div className="donate_option_btn_wrapper">
+              <button className='donate_one_time_btn'>One-Time</button>
+              <button className='donate_montly_btn'>Monthly</button>
+            </div>
+          </div>
+          <div className="donate_donation_type">
+            Donation Type
+            <select name="" id="">ywgwg</select>
+          </div>
+          <div className="donate_donation_type">
+            Donation Amount
+            <select name="" id="">ywgwg</select>
+          </div>
+          <h2>Contact Details</h2>
+          <div className="donate_name_wrapper">
+            <div className='donate_first_name'>
+              First Name
+              <input type="text" />
+            </div>
+            <div className='donate_first_name'>
+              Last Name
+              <input type="text" />
+            </div>
+          </div>
+          <div className="donate_email_wrapper">
+            Email
+            <input type="text" />
+          </div>
+          <div className="donate_cancel_wrapper">
+            <button className='donate_cancel_btn'>Cancel</button>
+            <button className='donate_checkout_btn'>Checkout</button>
+          </div>
         </div>
       </div>
     </div>
