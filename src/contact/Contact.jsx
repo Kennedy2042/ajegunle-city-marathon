8be924affdd9cd85033a2c6d18fab62abe2a8663
Kennedy2042 <ogbonnakennedy@gmail.com>
2024-06-19 22:15:05 +0100

    98% done with the gallery page

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -6,16 +6,32 @@ const Contact = () => {
       <div className="contact_us_hero_wrapper">
         CONTACT US
       </div>
-      <div className="get_intouch_wrapper">
-        <div className="get_intouch_body">
-          <div className="get_intouch">
+      <div className="contactUs_get_intouch_wrapper">
+        <div className="contactUs_get_intouch_body">
+          <div className="contactUs_get_intouch">
             <h2>Get in Touch</h2>
             <h1>RUN WITH US</h1>
             <p>Our dedicated team is here to help. If you have any questions or need assistance, don’t hesitate to get in touch.</p>
-            <div className="get_intouch_email_wrapper"></div>
+            <div className="contactUs_get_intouch_name_wrapper">
+              <input type="text" placeholder='First Name*'/>
+              <input type="text" placeholder='Last Name*'/>
+            </div>
+            <div className="contactUs_get_intouch_email_wrapper">
+              <input type="text" placeholder='Email*'/>
+            </div>
+            <div className="contactUs_get_intouch_email_wrapper">
+              <input type="text" placeholder='Phone Number*'/>
+            </div>
+            <div className="contactUs_get_intouch_textArea_wrapper">
+              <textarea name="" id=""></textarea>
+            </div>
+            <div className="contactUs_get_intouch_btn_wrapper">
+              <button>Send Message</button>
+            </div>
           </div>
         </div>
       </div>
+      <div className="contactUs_get_intouch_map"></div>
     </div>
   )
 }
--- src/gallery/Gallery.jsx
+++ src/gallery/Gallery.jsx
@@ -1,8 +1,85 @@
 import './Gallery.css'
+import gallery9 from '../assets/gallery9.png'
+import { FaAngleRight } from 'react-icons/fa6'
+import gallery1 from '../assets/gallery1.png'
+import gallery3 from '../assets/gallery3.png'
+
 
 const Gallery = () => {
+
+  const GalleryCard = (props) => {
+    return (
+      <div className='gallery_card_body'>
+        <div className="gallery_card_image_wrapper"
+          style={{ backgroundImage: `url(${props.img})` }}>
+          {/* <img src={props.img} alt="" className='gallery_card_image_wrapper_img'/> */}
+        </div>
+        <div className="gallery_card_image_content">
+          {props.content}
+        </div>
+      </div>
+    )
+  }
+
+  const GalleryWhyYouShouldJoincard = (props) => {
+    return (
+      <div className='gallery_why_body'>
+        <div className="gallery_why_icon_div">
+          <FaAngleRight color='white' />
+        </div>
+        <div className="gallery_why_article">
+          <h1 className='gallery_why_article_h1'>{props.H1}</h1>
+          <p className='gallery_why_article_p'>{props.P}</p>
+        </div>
+      </div>
+    )
+  }
+
+
   return (
-    <div>Gallery</div>
+    <div className='gallery_main_body'>
+      <div className="gallery_hero_wrapper"></div>
+      <div className="gallery_hero_writeUp_wrapper">
+        <h1>OUR GALLERY</h1>
+        <h2>Feeling Fit is the joy you get after running</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+      </div>
+      <div className="gallery_card_wrapper">
+        <GalleryCard  content='young-happy-athletic-women-running-a-marathon-in-nature-'/>
+        <GalleryCard  content='Happy-male-athlete-sprinter-in-sport-clothes-emotionally-rejoices-victory-at-the-running-track'/>
+        <GalleryCard  content='Friends running and exercise in city with training workout and cardio for health and wellness'/>
+        <GalleryCard  content='A young male athlete launching off the start line'/>
+        <GalleryCard  content='Marathon race runner running on a city road'/>
+        <GalleryCard  content='Two-male-runners-running-city-marathon-race-athletes-jogging-on-asphalt-road'/>
+      </div>
+      <div className="gallery_if_you_feel_container">
+        <div className="gallery_if_you_feel_wrapper">
+          <div className="gallery_if_you_feel_body">If you feel like feeling heaven like, Run and feel your breath and heartbeat</div>
+        </div>
+      </div>
+      <div className="gallery_make_the_diff_wrapper">
+        <div className="gallery_make_the_diff_body">
+          <div className="gallery_make_the_diff_image_wrapper">
+            <div className='gallery_make_the_diff_image_div'>
+              <img src={gallery9} alt="athlete" />
+            </div>
+            <div className='gallery_make_the_diff_content'>
+              <h1>Make The Difference</h1>
+            </div>
+          </div>
+          <div className="gallery_why_you_shouldJoin_wrapper">
+            <p className='gallery_why_you_shouldJoin_wrapper_P'>WHY YOU SHOULD JOIN US</p>
+            <h1 className='gallery_why_you_shouldJoin_wrapper_H1'>Run unintentionally, and feel the difference</h1>
+            <div className="gallery_why_you_shouldJoin_card_wrapper">
+              <GalleryWhyYouShouldJoincard H1='Be Healthy ' P='Per luctus accumsan dictumst duis orci finibus facilisi.' />
+              <GalleryWhyYouShouldJoincard H1='Be Strong' P='Per luctus accumsan dictumst duis orci finibus facilisi.' />
+              <GalleryWhyYouShouldJoincard H1='Be Faster ' P='Per luctus accumsan dictumst duis orci finibus facilisi.' />
+              <GalleryWhyYouShouldJoincard H1='Be One of Us' P='Per luctus accumsan dictumst duis orci finibus facilisi.' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
