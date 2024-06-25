f751068e9a861852484a7d948893a51548f9cbd9
Kennedy2042 <ogbonnakenedy@gmail.com>
2024-06-25 11:20:57 +0100

    about Prizes added

--- src/about/prizes/Prizes.jsx
+++ src/about/prizes/Prizes.jsx
@@ -1,9 +1,149 @@
 import { GeneralHero, GeneralLearnMore, GeneralRegister } from '../general_info/GeneralInfo'
+import './Prizes.css'
+import athlete from '../../assets/generalInfo2.png'
+
+
+
 
 const Prizes = () => {
     return (
-        <div>
-            <GeneralHero Title={"Prize"} H1={"Prizes for \nAjegunle City youth Marathon"}/>
+        <div className='prizes_mainBody'>
+            <GeneralHero Title={"Prize"} H1={"Prizes for \nAjegunle City youth Marathon"} />
+            <div className="prizes_article_section_mainBody">
+                <div className="prizes_article_section_wrapper">
+                    <div className="prizes_article_section_wrapper_img_container">
+                        <img src={athlete} alt="Athlete" className='prizes_article_section_wrapper_img' />
+                    </div>
+                    <div className="prizes_article_section_wrapper_article_container">
+                        <h1>Prizes</h1>
+                        <p>Participants regardless of diversities shall get the following awards for finishing each of these mileposts:</p>
+                        <table style={{ border: '1px solid blue' }}>
+                            <thead>
+                                <tr>
+                                    <td>Winners</td>
+                                    <td>Category</td>
+                                    <td>Cash Prizes (NGN)</td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <th>Wheelchair and Hand crank
+                                        (Male and Female 18 – 29 years)
+                                    </th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1 st Prize</td>
+                                    <td></td>
+                                    <td>300,000/=</td>
+                                </tr>
+                                <tr>
+                                    <td>2 nd Prize</td>
+                                    <td></td>
+                                    <td>200,000/=</td>
+                                </tr>
+                                <tr>
+                                    <td>3 rd Prize</td>
+                                    <td></td>
+                                    <td>100,000/=</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <th>Open Division and Corporate Division – Male category</th>
+                                    <th></th>
+                                </tr>
+                                <tr>
+                                    <td>1 st Prize</td>
+                                    <td></td>
+                                    <td>400,000/=</td>
+                                </tr>
+                                <tr>
+                                    <td>2 nd Prize</td>
+                                    <td></td>
+                                    <td>300,000/=</td>
+                                </tr>
+                                <tr>
+                                    <td>3 rd Prize</td>
+                                    <td></td>
+                                    <td>200,000/=</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <th>Open Division and Corporate Division - Female category</th>
+                                    <th></th>
+                                </tr>
+                                <tr>
+                                    <td>1 st Prize</td>
+                                    <td></td>
+                                    <td>300,000/=</td>
+                                </tr>
+                                <tr>
+                                    <td>2 nd Prize</td>
+                                    <td></td>
+                                    <td>200,000/=</td>
+                                </tr>
+                                <tr>
+                                    <td>3 rd Prize</td>
+                                    <td></td>
+                                    <td>100,000/=</td>
+                                </tr>
+                                <tr>
+                                    <td>4 th – 23rd Prize</td>
+                                    <td>Other 20 best Finishers (50,000/Finisher)</td>
+                                    <td>50,000/= 1M</td>
+                                </tr>
+                                <tr>
+                                    <td>All finishers</td>
+                                    <td></td>
+                                    <td>Certificates of participation</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <th>VVIP/VIP Division</th>
+                                    <th></th>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td>Special Certificates of sponsorship</td>
+                                    <td>Special Branded Gift Packaged</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <th></th>
+                                    <th>Other Awards</th>
+                                    <th></th>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td>Special Certificates of sponsorship and Awards</td>
+                                    <td>Special Branded Gift Packaged</td>
+                                </tr>
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+            </div>
             <GeneralRegister />
             <GeneralLearnMore />
         </div>
--- src/about/raceResult/RaceResult.jsx
+++ src/about/raceResult/RaceResult.jsx
@@ -1,9 +1,47 @@
 import { GeneralHero, GeneralLearnMore, GeneralRegister } from '../general_info/GeneralInfo'
+import './RaceResult.css'
+import athlete from '../../assets/generalInfo2.png'
+
+
+const RaceResultLink = ({ H5, Span }) => {
+    return (
+        <>
+            <h5 className='race_result_H5'>{H5} <span><a href="">{Span}</a></span></h5>
+        </>
+    )
+}
 
 const RaceResult = () => {
     return (
-        <div>
-            <GeneralHero Title={"Race Results"} H1={"Race results for \nAjegunle City youth Marathon"}/>
+        <div className='race_result_mainBody'>
+            <GeneralHero Title={"Race Results"} H1={"Race results for \nAjegunle City youth Marathon"} />
+            <div className="race_result_mainBody_article_section_mainBody">
+                <div className="race_result_mainBody_article_section_wrapper">
+                    <div className="race_result_mainBody_article_section_wrapper_img_container">
+                        <img src={athlete} alt="Athlete" className='race_result_mainBody_article_section_wrapper_img' />
+                    </div>
+                    <div className="race_result_mainBody_article_section_wrapper_article_container">
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                        <RaceResultLink H5={"2024 Race Results for ACYM Female – 5km –"} Span={"Click Here"} />
+                    </div>
+                </div>
+            </div>
             <GeneralRegister />
             <GeneralLearnMore />
         </div>
